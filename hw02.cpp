525aaee619b321b36b9b4b3963e33bb45d7dc3a9
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-16 17:41:37 -0800

    Create hw02.cpp

---
+++ hw02.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : hw02.cpp
+// Author      : Jack Schmid
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include "hw02.h"
+
+
+int main() {
+	cout << "" << endl; // prints 
+	return 0;
+}
+
+for(rank)
+	for(suit)
+		deck[i].suit = suitType(suit)
+		deck[i].rank = rankType(rank)
