efdfa0c220606e4f71fb9279073da56a93e0a28c
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-16 17:41:53 -0800

    Create hw02.h

---
+++ hw02.h
@@ -0,0 +1,42 @@
+/*
+ * hw02.h
+ *
+ *  Created on: Jan 16, 2020
+ *      Author: jschmid20
+ */
+
+#ifndef HW02_H_
+#define HW02_H_
+
+#include <iostream>
+using namespace std;
+const string Suits[] = {"Hearts", "Diamonds", "Spades", "Clubs"}
+const string Types[] = {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+typedef enum {
+	Hearts, Diamonds, Spades, Clubs
+} suitType;
+
+typedef enum {
+	One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace
+} rankType;
+
+
+class Card{
+public:
+
+private:
+
+protected:
+
+};
+
+class Deck{
+public:
+
+private:
+
+protected:
+
+};
+
+#endif /* HW02_H_ */
