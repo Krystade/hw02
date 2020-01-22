5c2aada60f271d83b841ab37e8f7096de89bc7c6
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-21 18:00:12 -0800

    Update Deck.h

--- Deck.h
+++ Deck.h
@@ -1,1 +1,30 @@
+/*
+ * Deck.h
+ *
+ *  Created on: Jan 21, 2020
+ *      Author: jschmid20
+ */
 
+#ifndef DECK_H_
+#define DECK_H_
+
+#include <iostream>
+#include "hw02.h"
+#include "Card.h"
+using namespace std;
+
+class Deck{
+public:
+	Deck();
+	~Deck();
+
+	Card getCard(int i) const {return cards[i];}
+	void perfectShuffle();
+	void print();
+private:
+	Card cards[DECK_SIZE];
+protected:
+
+};
+
+#endif /* DECK_H_ */
