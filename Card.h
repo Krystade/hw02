31ce543b087ed11944307f324af9c57ad3837553
Krys <21113012+Krystade@users.noreply.github.com>
2020-01-21 17:06:43 -0800

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,1 +1,34 @@
+/*
+ * Card.h
+ *
+ *  Created on: Jan 21, 2020
+ *      Author: jschmid20
+ */
 
+#ifndef CARD_H_
+#define CARD_H_
+
+#include <iostream>
+using namespace std;
+
+class Card{
+public:
+	Card();
+	Card(string startRank, string startSuit);
+	~Card();
+	void setSuit(string newSuit){suit = newSuit;}
+	string getSuit(){return suit;}
+	void setRank(string newRank){rank = newRank;}
+	string getRank(){return rank;}
+
+	void print(){cout << "Rank: " << getRank() << "\tSuit: " << getSuit() << endl;};
+private:
+	string suit;
+	string rank;
+protected:
+
+};
+
+
+
+#endif /* CARD_H_ */
