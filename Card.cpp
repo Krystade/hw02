e645e2e1bc98e457ee8fca1364d1e199f93cbb09
Krys <21113012+Krystade@users.noreply.github.com>
2020-02-19 20:12:26 -0800

    Update Card.cpp

--- Card.cpp
+++ Card.cpp
@@ -7,15 +7,17 @@
 
 #include "Card.h"
 #include "hw02.h"
-
+//Default Card Constructor
 Card::Card(){
 	rank = ranks[0];
 	suit = suits[0];
 }
+//Overloaded Card Constructor
 Card::Card(string startRank, string startSuit){
 	rank = startRank;
 	suit = startSuit;
 }
+//Card Destructor
 Card::~Card(){}
 
 
